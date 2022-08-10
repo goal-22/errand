63ffc64c89de6694308d54561e93a03ab4316816
lige <954201065@qq.com>
2022-08-10 17:28:17 +0800

    Initial commit

---
+++ src/main/java/com/goal/errand/ErrandApplication.java
@@ -0,0 +1,13 @@
+package com.goal.errand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goal/errand/ErrandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goal.errand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
